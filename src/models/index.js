284455bf0cdeea068fe8700640c3d3c378beba88
Santiago Guamanquispe <santiago_davidgm@hotmail.com>
2024-04-25 22:59:55 -0500

    advance4

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,19 @@
 const User = require("./User")
 const Category = require("./Category")
 const Product = require("./Product")
+const Cart = require("./Cart")
 
+//product -> categoryId
+
+Product.belongsTo(Category)
+Category.hasMany(Product)
+
+//cart -> userId
+
+Cart.belongsTo(User)
+User.hasMany(Cart)
+
+//cart -> productId
+
+Cart.belongsTo(Product)
+Product.hasMany(Cart)
